c25cc78b3b0be158090c10963adc74cb88fe9204
adnanjaf <adnan.jafferjee@gmail.com>
2018-11-07 16:55:45 -0500

    Prototype HSV Code

--- PiCode/HSV_CV.py
+++ PiCode/HSV_CV.py
@@ -1,69 +1,85 @@
-def filterColor(self, img, ranges=(255,25,255)):
+#!/usr/bin/env python2
+
+# Code to detect Xo and PsiR from pictures of Penn Park roads taken from Pi Camera 
+import cv2
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from HSV_Functions import filterColor
+
+# Read in image from directory of Penn Park images
+imgBGR = cv2.imread('/Users/adnanjafferjee/ESE421/PennParkImages/curvingRoad.jpg')
+imgGray = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2GRAY)
+height, width = imgGray.shape
+ranges=(255,25,255) # Range of acceptable HSV values
 frontpoint = (0.5, 0.1) # fractions of total resolution in (x,y)
 avgsize = 5 # number of pixels to average
 blursize = 25
-# Convert to HSV
-imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# Convert image to HSV
+imghsv = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2HSV)
+# Blur image
 imghsv = cv2.GaussianBlur(imghsv,(blursize,blursize),0)
+
+## THIS CODE IS COURTESY OF RYAN KORTVELESY
 # Get color of road
-frontpoint = (int(imghsv.shape[0]*(1-frontpoint[1])), int(imghsv.shape[1]*frontpoint[0]))
-frontregion = imghsv[frontpoint[0]-avgsize//2:frontpoint[0]+avgsize//2, frontpoint[1]-avgsize//2:frontpoint[1]+avgsize//2, 0:3]
+frontpoint = (int(imghsv.shape[0]*(1-frontpoint[1])), 
+    int(imghsv.shape[1]*frontpoint[0]))
+frontregion = imghsv[frontpoint[0]-avgsize//2:frontpoint[0]+avgsize//2, 
+    frontpoint[1]-avgsize//2:frontpoint[1]+avgsize//2, 0:3]
 avgcolor = np.average(np.average(frontregion,axis=1),axis=0).flatten()
-# Color Threshold
-lowerthres = np.array([max(avgcolor[0]-ranges[0], 0), max(avgcolor[1]-ranges[1], 0), max(avgcolor[2]-ranges[2], 0)], dtype=np.uint8)
-upperthres = np.array([min(avgcolor[1]+ranges[0], 255), min(avgcolor[1]+ranges[1], 255), min(avgcolor[2]+ranges[2], 255)], dtype=np.uint8)
+
+# Create a mask of the image that separates all pixels that are within the roads
+# color threshold
+lowerthres = np.array([max(avgcolor[0]-ranges[0], 0),
+    max(avgcolor[1]-ranges[1], 0), max(avgcolor[2]-ranges[2], 0)], dtype=np.uint8)
+upperthres = np.array([min(avgcolor[1]+ranges[0], 255), 
+    min(avgcolor[1]+ranges[1], 255), min(avgcolor[2]+ranges[2], 255)], dtype=np.uint8)
 mask = cv2.inRange(imghsv, lowerthres, upperthres)
-return mask
+## END RYAN'S CODE
 
- 
+# Perform Canny edge detection 
+edges = cv2.Canny(mask, 100,255)
 
- 
+# Filter detected edges for potential road edges
+minLineLength = 10
+maxLineGap = 1
+lines = cv2.HoughLinesP(edges,rho=1,theta=np.pi/180,threshold=50,
+    minLineLength=minLineLength,maxLineGap=maxLineGap)
 
-def hull(self,mask):
-# Get contours
-mask.dtype = np.uint8
-im2, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# Get largest contour
-maxarea = 0
-maxcontour = contours[0]
-for i, contour in enumerate(contours):
-area = cv2.contourArea(contour)
-if area > maxarea:
-maxarea = area
-maxcontour = contour
-# Simplify contour
-epsilon = 0.06*self.res[0]
-simplecontour = cv2.approxPolyDP(maxcontour,epsilon,True)
-return simplecontour
+CAMERA_HEIGHT = 13
+CAMERA_FOCAL_LENGTH = 1098.88
 
- 
+for x in range(0, len(lines)):
+    for x1,y1,x2,y2 in lines[x]:
+        cv2.line(imgBGR,(x1,y1),(x2,y2),(0,0,255),2) # draw the line on the original image
+        slope_image = (x2 - x1) * 1.0/ (y2 - y1)
+        x_intercept_computer_coords = x1 - (slope_image * y1)
+        x_intercept_centered_coords = slope_image * (height/2) + x_intercept_computer_coords - (width/2)
+        
+        offset = slope_image * CAMERA_HEIGHT
+        psi_r = math.degrees(math.atan(x_intercept_centered_coords/CAMERA_FOCAL_LENGTH))
+        if y2 > 250:
+            print("Slope in image: {} Unshifted Intercept: {} Shifted Intercept: {} x1: {} x2: {} y1: {} y2: {}".format(slope_image, x_intercept_computer_coords, x_intercept_centered_coords, x1, x2, y1, y2))
+            print("Estimated Offset: {0:.2f}".format(offset))
+            print("Estimated Psi_r: {0:.2f}".format(psi_r))
 
- 
+# Plot progressive road detection steps
+fig = plt.figure()
+a = fig.add_subplot(2, 2, 1)
+imgplot = plt.imshow(imgBGR)
+a.set_title('RGB ')
+a = fig.add_subplot(2, 2, 2)
+imgplot = plt.imshow(imghsv)
+a.set_title('HSV')
+a = fig.add_subplot(2, 2, 3)
+imgplot = plt.imshow(mask)
+a.set_title('HSV Thresholded')
+a = fig.add_subplot(2, 2, 4)
+imgplot = plt.imshow(edges)
+a.set_title('HSV Edges')
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+plt.show(block=True)
 
-def segment(self,hull):
-# Find best segment in the hull
-minlength = self.res[0]/2.5
-best = 0
-segment = None
-segangle = 0
-for i in range(1,hull.shape[0]):
-# Check that line is not horizontal or vertical
-if (hull[i,0,0] != hull[i-1,0,0]) and (hull[i,0,1] != hull[i-1,0,1]):
-# Check that the line is above a threshold length
-length = np.linalg.norm(hull[i,0,:]-hull[i-1,0,:])
-if length > minlength:
-# Get angle
-angle = -atan2(hull[i,0,0] - hull[i-1,0,0], hull[i,0,1] - hull[i-1,0,1])
-if angle > pi/2:
-angle -= pi
-elif angle < -pi/2:
-angle += pi
-# Choose best segment according to quality function
-quality = cos(angle/2) * length
-if quality > best:
-best = quality
-segment = (hull[i-1,0,:], hull[i,0,:])
-segangle = angle
-# returns tuple of 2 element np arrays in img coordinates. transform to xy with self.img2xy(points)
-return segment
-
+# TODO: Feedback "expected" value of PsiR and Xo to compare with detected (low pass filter)
