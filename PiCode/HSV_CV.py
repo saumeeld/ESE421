9f4425aa815613cf226649d4704a9331fce27ddb
adnanjaf <adnan.jafferjee@gmail.com>
2018-11-11 16:54:59 -0500

    Bogey commit message

--- PiCode/HSV_CV.py
+++ PiCode/HSV_CV.py
@@ -25,7 +25,7 @@ import matplotlib.pyplot as plt
 # Change this to where the image 
 
 # Read on
-imgBGR = cv2.imread('/Users/adnanjafferjee/ESE421/PennParkImages/Picture 41.jpg')
+imgBGR = cv2.imread('/Users/adnanjafferjee/ESE421/PennParkImages/curvingRoad.jpg')
 imgGray = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2GRAY)
 height, width = imgGray.shape
 ranges=(255,25,255) # Range of acceptable HSV values
@@ -60,43 +60,52 @@ mask = cv2.inRange(imghsv, lowerthres, upperthres)
 heightOffset = height/2
 widthOffset = width/2
 edges = cv2.Canny(mask[heightOffset:height, widthOffset:width], 100,255)
+
+# Print out dimensions of cropped image
 cropHeight,cropWidth = edges.shape
-print(cropHeight,cropWidth)
 # Filter detected edges for potential road edges
 minLineLength = 40
 maxLineGap = 30
 lines = cv2.HoughLinesP(edges,rho=1,theta=np.pi/180,threshold=20,
     minLineLength=minLineLength,maxLineGap=maxLineGap)
 
+# Empirically determined camera constants
 CAMERA_HEIGHT = 13
 CAMERA_FOCAL_LENGTH = 1098.88
 
-# WIP Code that will select the "most likely line" based on heuristics
-# y2max = 0
-# chosenLine = []
-# for x in range(0, len(lines)):
-#     for x1,y1,x2,y2 in lines[x]:
-#         slope_image = (x2 - x1) * 1.0/ (y2 - y1)
-#         if slope_image < 0:
-#             if y2 > y2max:
-#                 y2max = y2
-#                 chosenLine = lines[x]
-
-# x1 =chosenLine[0]
-# x2 =chosenLine[1]
-# y1 =chosenLine[3]
-# y2 =chosenLine[4]
-
+y2max = 0
 for x in range(0, len(lines)):
     for x1,y1,x2,y2 in lines[x]:
-        # Draw the line on the original image
-        cv2.line(imgBGR,(widthOffset+x1,heightOffset+y1),(widthOffset+x2,heightOffset+y2),(255,0,0),2) 
+        x1 = x1 + widthOffset
+        x2 = x2 + widthOffset
+        y1 = y1 + heightOffset
+        y2 = y2 + heightOffset
         slope_image = (x2 - x1) * 1.0/ (y2 - y1)
         x_intercept_computer_coords = x1 - (slope_image * y1)
         x_intercept_centered_coords = slope_image * (height/2) + x_intercept_computer_coords - (width/2)
-        
-        offset = slope_image * CAMERA_HEIGHT
-        psi_r = math.degrees(math.atan(x_intercept_centered_coords/CAMERA_FOCAL_LENGTH))
+
+        if slope_image > 0 and y2 > y2max:
+            y2max = y2
+            chosenLine = [x1,y1,x2,y2]
+            # Calculate Xo and PsiR
+            offset = slope_image * CAMERA_HEIGHT
+            psi_r = math.degrees(math.atan(x_intercept_centered_coords/CAMERA_FOCAL_LENGTH))
+
+# TODO: Write error handler for NameError (if chosenLine is undefined)
+x1 = chosenLine[0]
+y1 = chosenLine[1]
+x2 = chosenLine[2]
+y2 = chosenLine[3]
+
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+fontScale              = 1
+fontColor              = (255,255,255)
+lineType               = 2
+cv2.putText(imgBGR,"Estimated Offset: {0:.2f}".format(offset),(50,400),font,fontScale,fontColor,lineType)
+cv2.putText(imgBGR,"Estimated Psi_R: {0:.2f}".format(psi_r),(50,450),font,fontScale,fontColor,lineType)
+
+# Draw the line on the original image
+cv2.line(imgBGR,(x1,y1),(x2,y2),(255,0,0),2)
 
 print("Slope in image: {} Unshifted Intercept: {} Shifted Intercept: {} x1: {} x2: {} y1: {} y2: {}".format(slope_image, x_intercept_computer_coords, x_intercept_centered_coords, x1, x2, y1, y2))
 print("Estimated Offset: {0:.2f}".format(offset))
@@ -106,25 +115,25 @@ print("Estimated Psi_r: {0:.2f}".format(psi_r))
 plt.imshow(imgBGR)
 plt.title('Detected Road Edge')
 
-fig = plt.figure()
-a = fig.add_subplot(2, 2, 1)
-# Original color image with detected line plotted on it
-imgplot = plt.imshow(imgBGR)
-a.set_title('RGB ')
-a = fig.add_subplot(2, 2, 2)
-# Image in HSV space
-imgplot = plt.imshow(imghsv)
-a.set_title('HSV')
-a = fig.add_subplot(2, 2, 3)
-# Thresholded HSV Mask
-imgplot = plt.imshow(mask)
-a.set_title('HSV Thresholded')
-a = fig.add_subplot(2, 2, 4)
-# Edges detected within region of interest of the thresholded mask
-imgplot = plt.imshow(edges)
-a.set_title('HSV Edges')
-plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
-                    wspace=0.35)
+# fig = plt.figure()
+# a = fig.add_subplot(2, 2, 1)
+# # Original color image with detected line plotted on it
+# imgplot = plt.imshow(imgBGR)
+# a.set_title('RGB ')
+# a = fig.add_subplot(2, 2, 2)
+# # Image in HSV space
+# imgplot = plt.imshow(imghsv)
+# a.set_title('HSV')
+# a = fig.add_subplot(2, 2, 3)
+# # Thresholded HSV Mask
+# imgplot = plt.imshow(mask)
+# a.set_title('HSV Thresholded')
+# a = fig.add_subplot(2, 2, 4)
+# # Edges detected within region of interest of the thresholded mask
+# imgplot = plt.imshow(edges)
+# a.set_title('HSV Edges')
+# plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+#                     wspace=0.35)
 plt.show(block=True)
 
 # TODO: Feedback "expected" value of PsiR and Xo to compare with detected (low pass filter)
