291891756c608d3692d23faffbd508219f88e59f
adnanjaf <adnan.jafferjee@gmail.com>
2018-11-28 15:36:13 -0500

    Added test code to read a single encoder

--- PiCode/HSV_CV_Platoon.py
+++ PiCode/HSV_CV_Platoon.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python2
 
 # Code to detect Xo and PsiR from pictures of Penn Park roads taken from Pi Camera 
 import cv2
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+
+
 # import picamera
 
 # UNCOMMENT ALL OF THIS IF YOU WANT TO ANALYZE PICTURES TAKEN BY THE PI
@@ -23,137 +24,153 @@ import matplotlib.pyplot as plt
 
 # Read in image from directory of Penn Park images
 # Change this to where the image 
+    
+def perform_image_transformations(imgBGR):
+    imgGray = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2GRAY)
+    
+    ranges=(20,90,255) # Range of acceptable HSV values
+    frontpoint = (0.5, 0.1) # fractions of total resolution in (x,y)
+    avgsize = 5 # number of pixels to average
+    blursize = 25
+    
+    # Convert image to HSV
+    imghsv = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2HSV)
+    # Blur image
+    imghsv = cv2.GaussianBlur(imghsv,(blursize,blursize),0)
+    
+    return imgGray, imghsv
 
-# Read on
-imgBGR = cv2.imread('/Users/adnanjafferjee/ESE421/PlatoonTestData/LinearOffset0".jpg')
-imgGray = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2GRAY)
-height, width = imgGray.shape
-ranges=(0,5,31) # Range of acceptable HSV values
-frontpoint = (0.5, 0.1) # fractions of total resolution in (x,y)
-avgsize = 5 # number of pixels to average
-blursize = 25
 
-# Convert image to HSV
-imghsv = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2HSV)
-# Blur image
-imghsv = cv2.GaussianBlur(imghsv,(blursize,blursize),0)
+def get_CV_results(imgBGR, imgGray, imghsv,edges,heightOffset,widthOffset,height,width):
+     # Print out dimensions of cropped image
+    cropHeight,cropWidth = edges.shape
+    # Filter detected edges for potential road edges
+    minLineLength = 30
+    maxLineGap = 30
+    lines = cv2.HoughLinesP(edges,rho=1,theta=np.pi/180,threshold=20,
+        minLineLength=minLineLength,maxLineGap=maxLineGap)
 
-## THIS CODE IS COURTESY OF RYAN KORTVELESY
-# Get color of road
-frontpoint = (int(imghsv.shape[0]*(1-frontpoint[1])), 
-    int(imghsv.shape[1]*frontpoint[0]))
-frontregion = imghsv[frontpoint[0]-avgsize//2:frontpoint[0]+avgsize//2, 
-    frontpoint[1]-avgsize//2:frontpoint[1]+avgsize//2, 0:3]
-avgcolor = np.average(np.average(frontregion,axis=1),axis=0).flatten()
+    # Empirically determined camera constants
+    CAMERA_HEIGHT = 13
+    CAMERA_FOCAL_LENGTH = 1098.88
 
-# Create a mask of the image that separates all pixels that are within the roads
-# color threshold
-lowerthres = np.array([max(avgcolor[0]-ranges[0], 0),
-    max(avgcolor[1]-ranges[1], 0), max(avgcolor[2]-ranges[2], 0)], dtype=np.uint8)
-upperthres = np.array([min(avgcolor[1]+ranges[0], 255), 
-    min(avgcolor[1]+ranges[1], 255), min(avgcolor[2]+ranges[2], 255)], dtype=np.uint8)
+    y2max = 0
+    for x in range(0, len(lines)):
+        for x1,y1,x2,y2 in lines[x]:
+            x1 = x1 + widthOffset
+            x2 = x2 + widthOffset
+            y1 = y1 + heightOffset
+            y2 = y2 + heightOffset
+            slope_image = (x2 - x1) * 1.0/ (y2 - y1)
+            x_intercept_computer_coords = x1 - (slope_image * y1)
+            x_intercept_centered_coords = slope_image * (height/2) + x_intercept_computer_coords - (width/2)
+            print("x1: {} x2: {} y1: {} y2: {}".format(x1, x2, y1, y2))
+            if y2 > y2max:
+                y2max = y2
+                chosenLine = [x1,y1,x2,y2]
+                # Calculate Xo and PsiR
+                offset = slope_image * CAMERA_HEIGHT
+                psi_r = math.degrees(math.atan(x_intercept_centered_coords/CAMERA_FOCAL_LENGTH))
+            
+            x1 = chosenLine[0]
+            x2 = chosenLine[1]
+            y1 = chosenLine[2]
+            y2 = chosenLine[3]
+            # Draw the line on the original image
+            cv2.line(imgBGR,(x1,y1),(x2,y2),(255,0,0),4)
+    
+    return offset, psi_r, chosenLine
 
-# Hard-coded thresholds for orange (Adnan)
-lowerthres = np.array([0,200,170], dtype=np.uint8)
-upperthres = np.array([30,255,255],dtype=np.uint8)
+def init_camera(photoHeight):
+    camera = picamera.PiCamera()
+    photoHeight = 540
+    camera.resolution = (16*photoHeight/9, photoHeight)
+    return camera
 
-mask = cv2.inRange(imghsv, lowerthres, upperthres)
-## END RYAN'S CODE
+def threshold_image(imgBGR,imgGray,imghsv):
+    ranges=(0,10,100) # Range of acceptable HSV values
+    frontpoint = (0.5, 0.1) # fractions of total resolution in (x,y)
+    avgsize = 5 # number of pixels to average
+    blursize = 25
+    height, width = imgBGR.shape[0:2]
 
-# Perform Canny edge detection on selected region of interest
-# (No offset for platoon image)
-heightOffset = height/2
-widthOffset = 0
-edges = cv2.Canny(mask[heightOffset:height, widthOffset:width], 100,255)
+    # Hard-coded thresholds for orange in hsv space
+    lowerthres = np.array([0,100,100], dtype=np.uint8)
+    upperthres = np.array([100,200,255], dtype=np.uint8)
+    mask = cv2.inRange(imghsv, lowerthres, upperthres)
 
-# Print out dimensions of cropped image
-cropHeight,cropWidth = edges.shape
-# Filter detected edges for potential road edges
-minLineLength = 20
-maxLineGap = 5
-lines = cv2.HoughLinesP(edges,rho=1,theta=np.pi/180,threshold=20,
-    minLineLength=minLineLength,maxLineGap=maxLineGap)
+    # Perform Canny edge detection on selected region of interest
+    # (Bottom right-hand corner of image)
+    heightOffset = 0
+    widthOffset = 0
+    edges = cv2.Canny(mask[heightOffset:height, widthOffset:width], 100,255)
+    return mask, edges,heightOffset,widthOffset,height,width
+
+def capture_image(camera, camstore_filename):
+    camera.capture(camstore_filename)
+    imgBGR = cv2.imread(camstore_filename) 
 
 # Plot debugging graphs
-b,g,r = cv2.split(imgBGR)       # get b,g,r
-imgRGB = cv2.merge([r,g,b]) 
-fig = plt.figure()
-plt.imshow(imgRGB)
-plt.title('Detected Road Edge')
-plt.show(block=True)
+def plot_data(imgBGR, imghsv, mask, edges):
+    b,g,r = cv2.split(imgBGR)       # get b,g,r
+    imgRGB = cv2.merge([r,g,b])
+    plt.imshow(imgRGB)
+    plt.title('Detected Road Edge')
 
-# Empirically determined camera constants
-CAMERA_HEIGHT = 13
-CAMERA_FOCAL_LENGTH = 1098.88
-fig = plt.figure()
-a = fig.add_subplot(2, 2, 1)
-# Original color image with detected line plotted on it
-imgplot = plt.imshow(imgBGR)
-a.set_title('BGR')
-a = fig.add_subplot(2, 2, 2)
-# Image in HSV space
-imgplot = plt.imshow(imghsv)
-a.set_title('HSV')
-a = fig.add_subplot(2, 2, 3)
-# Thresholded HSV Mask
-imgplot = plt.imshow(mask)
-a.set_title('HSV Thresholded')
-a = fig.add_subplot(2, 2, 4)
-# Edges detected within region of interest of the thresholded mask
-imgplot = plt.imshow(edges)
-a.set_title('HSV Edges')
-plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
-                    wspace=0.35)
-plt.show(block=False)
+    fig = plt.figure()
+    a = fig.add_subplot(2, 2, 1)
+    # Original color image with detected line plotted on it
+    imgplot = plt.imshow(imgRGB)
+    a.set_title('RGB')
+    a = fig.add_subplot(2, 2, 2)
+    # Image in HSV space
+    imgplot = plt.imshow(imghsv)
+    a.set_title('HSV')
+    a = fig.add_subplot(2, 2, 3)
+    # Thresholded HSV Mask
+    imgplot = plt.imshow(mask)
+    a.set_title('HSV Thresholded')
+    a = fig.add_subplot(2, 2, 4)
+    # Edges detected within region of interest of the thresholded mask
+    imgplot = plt.imshow(edges)
+    a.set_title('HSV Edges')
+    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                        wspace=0.35)
+    plt.show(block=True)
 
-y2max = 0
-for x in range(0, len(lines)):
-    for x1,y1,x2,y2 in lines[x]:
-        x1 = x1 + widthOffset
-        x2 = x2 + widthOffset
-        y1 = y1 + heightOffset
-        y2 = y2 + heightOffset
-        slope_image = (x2 - x1) * 1.0/ (y2 - y1)
-        x_intercept_computer_coords = x1 - (slope_image * y1)
-        x_intercept_centered_coords = slope_image * (height/2) + x_intercept_computer_coords - (width/2)
-        # Calculate Xo and PsiR
-        offset = slope_image * CAMERA_HEIGHT
-        psi_r = math.degrees(math.atan(x_intercept_centered_coords/CAMERA_FOCAL_LENGTH))
-        # Draw the line on the original image
-        cv2.line(imgBGR,(x1,y1),(x2,y2),(255,0,0),4)
-        print("Slope in image: {} Unshifted Intercept: {} Shifted Intercept: {} x1: {} x2: {} y1: {} y2: {}".format(slope_image, x_intercept_computer_coords, x_intercept_centered_coords, x1, x2, y1, y2))
-        print("Estimated Offset: {0:.2f}".format(offset))
-        print("Estimated Psi_r: {0:.2f}".format(psi_r))
+def debug_chosen_line(offset, psi_r, chosenLine, imgBGR):
+    x1 = chosenLine[0]
+    y1 = chosenLine[1]
+    x2 = chosenLine[2]
+    y2 = chosenLine[3]
 
-        # if slope_image > 0 and y2 > y2max:
-        #     y2max = y2
-        #     chosenLine = [x1,y1,x2,y2]
-        #     # Calculate Xo and PsiR
-        #     offset = slope_image * CAMERA_HEIGHT
-        #     psi_r = math.degrees(math.atan(x_intercept_centered_coords/CAMERA_FOCAL_LENGTH))
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale              = 1
+    fontColor              = (255,255,255)
+    lineType               = 2
+    cv2.putText(imgBGR,"Estimated Offset: {0:.2f}".format(offset),(50,400),font,fontScale,fontColor,lineType)
+    cv2.putText(imgBGR,"Estimated Psi_R: {0:.2f}".format(psi_r),(50,450),font,fontScale,fontColor,lineType)
 
-
-
-# TODO: Write error handler for NameError (if chosenLine is undefined)
-# x1 = chosenLine[0]
-# y1 = chosenLine[1]
-# x2 = chosenLine[2]
-# y2 = chosenLine[3]
-
-# font                   = cv2.FONT_HERSHEY_SIMPLEX
-# fontScale              = 1
-# fontColor              = (255,255,255)
-# lineType               = 2
-# cv2.putText(imgBGR,"Estimated Offset: {0:.2f}".format(offset),(50,400),font,fontScale,fontColor,lineType)
-# cv2.putText(imgBGR,"Estimated Psi_R: {0:.2f}".format(psi_r),(50,450),font,fontScale,fontColor,lineType)
-
-# # Draw the line on the original image
-# cv2.line(imgBGR,(x1,y1),(x2,y2),(255,0,0),4)
-
-# print("Slope in image: {} Unshifted Intercept: {} Shifted Intercept: {} x1: {} x2: {} y1: {} y2: {}".format(slope_image, x_intercept_computer_coords, x_intercept_centered_coords, x1, x2, y1, y2))
-# print("Estimated Offset: {0:.2f}".format(offset))
-# print("Estimated Psi_r: {0:.2f}".format(psi_r))
-
-
+    print("x1: {} x2: {} y1: {} y2: {}".format(x1, x2, y1, y2))
+    print("Estimated Offset: {0:.2f}".format(offset))
+    print("Estimated Psi_r: {0:.2f}".format(psi_r))
+      
+def main():
+    CAMSTORE_FILENAME = 'piPicture.jpg'
+    PHOTO_HEIGHT = 540
+    # camera = init_camera(PHOTO_HEIGHT)
+    # imgBGR = capture_image(camera, CAMSTORE_FILENAME)
+    imgBGR = cv2.imread('/Users/adnanjafferjee/ESE421/PlatoonTestData/LinearOffset0".jpg')
+    imgGray, imghsv = perform_image_transformations(imgBGR)
+    edges,mask,heightOffset,widthOffset,height,width = threshold_image(imgBGR,imgGray,imghsv)
+    offset, psi_r, chosenLine = get_CV_results(imgBGR,imgGray,imghsv,edges,heightOffset,widthOffset,height,width)
+    debug_chosen_line(offset, psi_r, chosenLine, imgBGR)  
+    plot_data(imgBGR, imghsv, mask, edges)
+    return
+    
+if __name__ == "__main__":
+    main()
+    
 
 # TODO: Feedback "expected" value of PsiR and Xo to compare with detected (low pass filter)
+
